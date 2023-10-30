256c096ab8b754b94fc0a7fd0c8434fd638bd44c
hossein <Hossein.m.7782@gmail.com>
2023-10-30 19:13:04 +0330

    create model

---
+++ posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    STATUS_CHOICES=(
+        ('pub', 'published'),
+        ('drf','draft'),
+    )
+    title= models.CharField(max_length=50)
+    text= models.TextField()
+    author= models.ForeignKey("auth.User",on_delete=models.CASCADE)
+    date_time_created= models.DateTimeField( auto_now_add=True)
+    date_time_modified= models.DateTimeField(auto_now=True)
+    status=models.CharField(choices=STATUS_CHOICES, max_length=50)
