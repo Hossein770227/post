50bc56c8bbd6b21d27056c10b658fad74cb927f5
hossein <Hossein.m.7782@gmail.com>
2023-10-31 09:38:46 +0330

    post detail

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name= 'post_list'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name= 'post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -8,3 +8,12 @@ class PostListView(generic.ListView):
     model=Post
     template_name='posts/post_list.html'
     context_object_name='posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('date_time_created')
+    
+
+class PostDetailView(generic.DetailView):
+    model=Post
+    template_name='posts/post_detail.html'
+    context_object_name='post'
